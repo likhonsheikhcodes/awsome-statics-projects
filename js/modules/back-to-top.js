5f7ad3d61453de10d0c052269423736053214cbd
Likhon Sheikh <likhonsheikhcodes@gmail.com>
2025-01-13 03:17:15 +0600

    Create back-to-top.js

---
+++ js/modules/back-to-top.js
@@ -0,0 +1,16 @@
+export function initBackToTop() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 100) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+}
+
